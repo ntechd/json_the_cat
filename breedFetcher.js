a571142ec0aa974a7675c0acd87b49e9254092e0
Najma Duale <najmaduale1@gmail.com>
2021-12-03 17:10:45 +0000

    refractoring

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,4 +1,32 @@
 const request = require("request");
+
+const fetchBreedDescription = function(breedName , callback){
+	let url="https://api.thecatapi.com/v1/breeds/search?q="
+	url = url+breedName;
+
+	request(url,(error,response,body)=>{
+		if(error)
+		{
+			callback(error,null);
+		}
+	const data = JSON.parse(body);
+	if(data[0]===undefined)
+	{
+		callback(error,null);
+	}
+	else {
+	callback(null,data[0]["description"]);	
+	}
+	})
+
+};
+
+
+
+module.exports = {fetchBreedDescription};
+
+
+/*
 const arguments = process.argv.slice(2);
 let url="https://api.thecatapi.com/v1/breeds/search?q="
 url = url+arguments[0];
@@ -17,4 +45,4 @@ if(data[0]===undefined)
 	return;
 }
 console.log(data[0]['description']);	
-})
+})*/
